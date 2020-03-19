5c4c180912abe9f648df1764ea7f66b092a08ac7
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-19 19:38:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,27 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+    subtraction = num1 - num2
+    puts subtraction
+    subtraction
 end
 
 def division(num1, num2)
-
+    division = num1 / num2
+    puts division
+    division
 end
 
 def multiplication(num1, num2)
-
+    multiplication = num1 * num2
+    puts multiplication
+    multiplication
 end
 
 def modulo(num1, num2)
-
+    modulo = (num1/num2)%
 end
 
 def square_root(num)
-
+    
 end
